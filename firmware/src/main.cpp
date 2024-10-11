411ac834d1bc11fc374e22efe77fa996be9c3024
Martin Fejt <martin@fejt.cz>
2024-10-11 23:56:36 +0200

    OLED Display + welcome message

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,15 +1,34 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 // Pins definitions
+#define SDA_PIN 21
+#define SCL_PIN 22
 #define BUZZER_PIN 27
 
-// Beep by the integrated buzzer
+// Display definitions
+#define DISPLAY_ADDRESS 0x3C
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 64
+#define DISPLAY_RESET -1
+Adafruit_SSD1306 display(DISPLAY_WIDTH, DISPLAY_HEIGHT, &Wire, DISPLAY_RESET);
+
+/*
+ * Beep by the integrated buzzer
+ * @param frequency - frequency of the sound
+ * @param duration - duration of the sound
+ */
 void beep(int frequency, int duration)
 {
   tone(BUZZER_PIN, frequency, duration);
 }
 
-// Play startup sound
+/*
+ * Play startup sound
+ */
 void startupSound()
 {
   beep(1000, 150);
@@ -23,7 +42,26 @@ void startupSound()
 
 void setup()
 {
+  // Serial output
   Serial.begin(115200);
+
+  // Display check
+  if (!display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDRESS))
+  {
+    Serial.println(F("Display allocation failed!"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+
+  // Welcome message
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.setCursor(20, 25);
+  display.println("BastlBot");
+  display.display();
+
+  // Startup sound
   startupSound();
 }
 
