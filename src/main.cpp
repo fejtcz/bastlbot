058f536cdae38fe8974629995a7c6b73806de428
Martin Fejt <martin@fejt.cz>
2024-12-16 23:28:10 +0100

    webserver + display messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_MotorShield.h>
 
@@ -29,6 +31,14 @@ Adafruit_DCMotor *motor1 = motorShield.getMotor(1);
 Adafruit_DCMotor *motor2 = motorShield.getMotor(2);
 Adafruit_DCMotor *motor3 = motorShield.getMotor(3);
 Adafruit_DCMotor *motor4 = motorShield.getMotor(4);
+
+// WiFi and Webserver definitions
+#define WIFI_SSID "BastlBot"
+#define WIFI_PASSWORD "B@stlbot24"
+IPAddress local_ip(10, 0, 0, 1);
+IPAddress gateway(10, 0, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+WebServer webserver(80); // Webserver on port 80
 
 /*
  * Movement functions
@@ -155,6 +165,181 @@ void playStartupSound()
   beep(2000, 350);
 }
 
+/*
+ * Display messages
+ */
+// Startup message
+void displayStartupMessage()
+{
+  display.setRotation(2);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.setCursor(20, 10);
+  display.println("BastlBot");
+  display.setTextSize(1);
+  display.setCursor(40, 30);
+  display.println("(Stepper)");
+  display.setCursor(10, 45);
+  display.println("IP Addr: 10.0.0.1");
+  display.setCursor(10, 55);
+  display.println("Bat. voltage: 0.00V"); // TODO - dodelat mereni napeti baterie
+  display.display();
+}
+// Waiting message
+void displayWaitingMessage()
+{
+  display.setRotation(2);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.setCursor(5, 10);
+  display.println("Waiting...");
+  display.display();
+}
+// Forward arrow
+void displayForwardArrow()
+{
+  display.setRotation(1);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(8);
+  display.setCursor(10, 35);
+  display.println(">");
+  display.display();
+}
+// Backward arrow
+void displayBackwardArrow()
+{
+  display.setRotation(3);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(8);
+  display.setCursor(10, 35);
+  display.println(">");
+  display.display();
+}
+// Left arrow
+void displayLeftArrow()
+{
+  display.setRotation(2);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(8);
+  display.setCursor(30, 5);
+  display.println("<");
+  display.display();
+}
+// Right arrow
+void displayRightArrow()
+{
+  display.setRotation(2);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(8);
+  display.setCursor(40, 5);
+  display.println(">");
+  display.display();
+}
+
+/*
+ * Run step instructions
+ * @param instructions - instructions to be run
+ */
+void runInstructions(String instructions)
+{
+  printMessage("Running instructions: " + String(instructions));
+
+  // Convert the instruction string to an array of characters
+  int length = instructions.length();
+  char instructionsArray[length + 1];
+  instructions.toCharArray(instructionsArray, length + 1);
+
+  // Run the instructions
+  for (int i = 0; i < length; i++)
+  {
+    switch (instructionsArray[i])
+    {
+    case '1':
+      displayForwardArrow();
+      stepForward();
+      break;
+    case '2':
+      displayLeftArrow();
+      stepLeft();
+      break;
+    case '3':
+      displayRightArrow();
+      stepRight();
+      break;
+    case '4':
+      displayBackwardArrow();
+      stepBackward();
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+/*
+ * Webserver pages
+ */
+// Homepage
+void webserverHomepage()
+{
+  String content = "<html>";
+  content += "<head>";
+  content += "<meta charset=\"UTF-8\">";
+  content += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+  content += "<title>BastlBot</title>";
+  content += "<style type=\"text/css\">";
+  content += "body {background-color: #f0f0f0;font-family: Arial, sans-serif;text-align: center;}";
+  content += "input {width: 80%;height: 50px;font-size: 2em;font-weight: bold;margin: 20px;text-align: center;}";
+  content += "table {margin: 20 auto;width: 50%;text-align: center;}";
+  content += "table button {width: 100px;height: 100px;margin: 10px;font-size: 2em;}";
+  content += "table button.middleone {width: 200px;}";
+  content += "table button.red {background-color: red;color: white;}";
+  content += "table button.green {background-color: green;color: white;}";
+  content += "</style>";
+  content += "<script>";
+  content += "let instructions = [];";
+  content += "function addInstruction(instruction, arrow) {instructions.push(instruction);document.querySelector('input[name=\"input\"]').value += ' ' + arrow + ' ';}";
+  content += "function clearInstructions() {instructions = [];document.querySelector('input[name=\"input\"]').value = '';}";
+  content += "function runInstructions() {instructionsString = instructions.join('');window.location.href = '/run?instructions=' + instructionsString;}";
+  content += "</script>";
+  content += "</head>";
+  content += "<body>";
+  content += "<h1>BastlBot</h1>";
+  content += "<input type=\"text\" name=\"input\" value=\"\" disabled>";
+  content += "<table>";
+  content += "<tr><td></td><td><button onclick=\"addInstruction(1, '&uarr;')\">&uarr;</button></td><td></td></tr>";
+  content += "<tr><td><button onclick=\"addInstruction(2, '&larr;')\">&larr;</button></td><td><button class=\"red\" onclick=\"clearInstructions()\">X</button></td><td><button onclick=\"addInstruction(3, '&rarr;')\">&rarr;</button></td></tr>";
+  content += "<tr><td></td><td><button onclick=\"addInstruction(4, '&darr;')\">&darr;</button></td><td></td></tr>";
+  content += "<tr><td colspan=\"3\"><button class=\"green middleone\" onclick=\"runInstructions()\">GO</button></td></tr>";
+  content += "</table>";
+  content += "</body>";
+  content += "</html>";
+  webserver.send(200, "text/html", content);
+  displayWaitingMessage();
+}
+// Run instructions
+void webserverRun()
+{
+  String instructions = webserver.arg("instructions");
+  if (instructions != "")
+  {
+    runInstructions(instructions);
+  }
+  webserver.sendHeader("Location", "/");
+  webserver.send(303);
+}
+// Not found page
+void webserverNotFound()
+{
+  webserver.send(404, "text/html", "<center><h1>HTTP 404</h1><h2>Not found!</h2></center>");
+}
+
 void setup()
 {
   // Serial output init
@@ -177,17 +362,23 @@ void setup()
     return;
   }
 
+  // WiFi initialization
+  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+  printMessage("WiFi AP started...");
+
+  // Webserver route definitions
+  webserver.on("/", webserverHomepage);
+  webserver.on("/run", webserverRun);
+  webserver.onNotFound(webserverNotFound);
+
+  // Webserver start
+  webserver.begin();
+  Serial.println("HTTP server started...");
+
   // OLED Startup message
-  display.setRotation(2);
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(2);
-  display.setCursor(20, 10);
-  display.println("BastlBot");
-  display.setTextSize(1);
-  display.setCursor(10, 55);
-  display.println("Bat. voltage: 0.00V"); // TODO - dodelat mereni napeti baterie
-  display.display();
+  displayStartupMessage();
 
   // Play startup sound
   playStartupSound();
@@ -195,4 +386,5 @@ void setup()
 
 void loop()
 {
+  webserver.handleClient();
 }
